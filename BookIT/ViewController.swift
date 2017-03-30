b7b8521b41431e9a8511154e8417333ca7a6d6ad
snyxiustechnologies <veeranti@snyxius.com>
2017-03-30 13:06:49 +0530

    Initial Commit

---
+++ BookIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookIT
+//
+//  Created by SRAVANKUMAR VEERANTI on 30/03/2017.
+//  Copyright © 2017 SRUJAN KUMAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
